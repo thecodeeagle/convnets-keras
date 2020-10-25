c991cc3405659225f1b61de981e9447f682f93e9
thecodeeagle <41933137+thecodeeagle@users.noreply.github.com>
2020-10-25 17:00:13 +0530

    Update customlayers.py

--- convnetskeras/customlayers.py
+++ convnetskeras/customlayers.py
@@ -3,7 +3,7 @@ from keras import backend as K
 from keras.engine import Layer
 from keras.layers.convolutional import Convolution2D
 from keras.layers.core import Lambda
-from keras.layers.core import Merge
+#from keras.layers.core import Merge
 
 
 def crosschannelnormalization(alpha=1e-4, k=2, beta=0.75, n=5, **kwargs):
@@ -53,17 +53,7 @@ def splittensor(axis=1, ratio_split=1, id_split=0, **kwargs):
     return Lambda(f, output_shape=lambda input_shape: g(input_shape), **kwargs)
 
 
-def convolution2Dgroup(n_group, nb_filter, nb_row, nb_col, **kwargs):
-    def f(input):
-        return Merge([
-                         Convolution2D(nb_filter // n_group, nb_row, nb_col)(
-                             splittensor(axis=1,
-                                         ratio_split=n_group,
-                                         id_split=i)(input))
-                         for i in range(n_group)
-                         ], mode='concat', concat_axis=1)
 
-    return f
 
 
 class Softmax4D(Layer):
